9b27d7969f7f91342f283d7fdd82e2683566a1b6
Dima Pinaev <dimapokemon005@gmail.com>
2022-11-30 22:14:20 +0300

    Решил задачу

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string[] CreateNewArray(string[] arr)
+{
+    string[] newArr = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newArr[i] = arr[i];
+        }
+    }
+    return newArr;
+}
+
+void ShowArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write(arr[i] + " ");
+
+    Console.WriteLine();
+}
+
+string[] array = {"abc", "abcd", "ab", "b", "bcdf"};
+ShowArray(array);
+ShowArray(CreateNewArray(array));
